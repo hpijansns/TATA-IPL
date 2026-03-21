10384cca139c4f1d1ecddc8f415cf88d87db458d
Free <mohitrajpura9@gmail.com>
2026-03-21 10:08:46 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,29 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-  getDatabase, 
-  ref as dbRef, 
-  onValue, 
-  set, 
-  push, 
-  update, 
-  remove, 
-  get 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, onValue, set, push, remove } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD6LXM57mJI-VLT3GXvimDjDaNNyNxlwro",
   authDomain: "book-my-show-b30d1.firebaseapp.com",
   databaseURL: "https://book-my-show-b30d1-default-rtdb.firebaseio.com",
   projectId: "book-my-show-b30d1",
-  storageBucket: "book-my-show-b30d1.firebasestorage.app",
+  storageBucket: "book-my-show-b30d1.appspot.com",
   messagingSenderId: "307666240968",
   appId: "1:307666240968:web:004906097839e002222cfd"
 };
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const db = getDatabase(app);
 
-// SAFE REF
-const ref = (path) => dbRef(database, path);
-
-export { database as db, ref, onValue, set, push, update, remove, get };
+export { db, ref, onValue, set, push, remove };
