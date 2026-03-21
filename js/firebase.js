46912f0cf2049faedc241b8f5c0b608c318f5c05
Free <mohitrajpura9@gmail.com>
2026-03-21 08:17:47 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { 
+  getDatabase, 
+  ref as dbRef, 
+  onValue, 
+  set, 
+  push, 
+  update, 
+  remove, 
+  get 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6LXM57mJI-VLT3GXvimDjDaNNyNxlwro",
+  authDomain: "book-my-show-b30d1.firebaseapp.com",
+  databaseURL: "https://book-my-show-b30d1-default-rtdb.firebaseio.com",
+  projectId: "book-my-show-b30d1",
+  storageBucket: "book-my-show-b30d1.firebasestorage.app",
+  messagingSenderId: "307666240968",
+  appId: "1:307666240968:web:004906097839e002222cfd"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// SAFE REF
+const ref = (path) => dbRef(database, path);
+
+export { database as db, ref, onValue, set, push, update, remove, get };
